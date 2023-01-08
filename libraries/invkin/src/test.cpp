4580955ec428ccdaffd0f73daa07f631c4446a5a
Alex-Stone-Github <stonealex145@gmail.com>
2023-01-08 13:52:13 -0600

    Simulation with inverse kinematics

--- libraries/invkin/src/test.cpp
+++ libraries/invkin/src/test.cpp
@@ -9,6 +9,30 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+double t = 0;
+bool is_tracing = false;
+std::vector<Vector> points;
+
+
+Vector lerp(Vector a, Vector b, double percent) {
+    return Vector(a.x + (b.x-a.x)*percent, a.y + (b.y-a.y)*percent);
+}
+Vector piecemeal(double percent) {
+    std::vector<Vector> ps = points;
+    if (points.size() > 1) {
+        // drain
+        while (ps.size() > 1) {
+            std::vector<Vector> new_ps;
+            for (int i = 1; i < ps.size(); i ++) {
+                new_ps.push_back(lerp(ps[i-1], ps[i], percent));
+            }
+            ps = new_ps;
+        }
+        return ps[0]; // return the only element
+    }
+    return Vector(0, 0);
+}
+
 int main() {
     // init
     std::cout << "Hello, World" << std::endl;
@@ -35,20 +59,13 @@ int main() {
     }
 
     // logic
-    std::vector<double> lengths = {100, 90};
+    std::vector<double> lengths = {200, 170};
     Arm arm(Vector(300, 300), lengths);
 
 
     // while loop
     while (true) {
-        // events
-        SDL_Event e;
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                goto quit;
-            }
-        }
-        // logic
+        // events / input
         double mouse_x;
         double mouse_y;
         {
@@ -57,7 +74,34 @@ int main() {
             mouse_x = mx;
             mouse_y = my;
         }
-        arm.move_to_target_with_anchoring(Vector(mouse_x, mouse_y));
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                goto quit;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN) {
+                points.push_back(Vector(mouse_x, mouse_y));
+            }
+            if (e.type == SDL_KEYDOWN) {
+                is_tracing = true;
+            }
+        }
+        // logic
+        if (!is_tracing) {
+            arm.move_to_target_with_anchoring(Vector(mouse_x, mouse_y));
+        }
+        else { // tracing
+            Vector target = piecemeal(t);
+            arm.move_to_target_with_anchoring(target);
+        }
+        if (t > 1) {
+            is_tracing = false;
+            points = {};
+            t = 0;
+        }
+        if (is_tracing) {
+            t += 0.0001;
+        }
         // render --- 
         // background
         SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
@@ -69,14 +113,37 @@ int main() {
             Vector end = segment.calculate_end();
             SDL_RenderDrawLine(renderer, segment.position.x, segment.position.y,
                     end.x, end.y);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_Rect r;
+            r.x = end.x - 3;
+            r.y = end.y - 3;
+            r.w = 6; r.h = 6;
+            SDL_RenderFillRect(renderer, &r);
         }
         // draw mouse
+        {
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_Rect r;
         r.x = mouse_x - 5;
         r.y = mouse_y - 5;
         r.w = 10; r.h = 10;
         SDL_RenderFillRect(renderer, &r);
+        }
+        // draw points
+        for (auto point : points) {
+            SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+            SDL_Rect r;
+            r.x = point.x - 5;
+            r.y = point.y - 5;
+            r.w = 10; r.h = 10;
+            SDL_RenderFillRect(renderer, &r);
+        }
+        // draw path
+        for (double i = 0; i < 1; i += 0.01) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+            Vector point = piecemeal(i);
+            SDL_RenderDrawPoint(renderer, point.x, point.y);
+        }
         // done
         SDL_RenderPresent(renderer);
     }
